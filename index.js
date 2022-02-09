d0e788f6b50e494e9cbe773a97d8b2b934035917
Taylor Huffman <taylor@crucialartists.com>
2022-02-09 14:53:52 -0600

    commit tree 0d039820d75d5222910e01f14cda1c4e96caaab0

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your classes here
+class Tree {
+    constructor(species) {
+        this.species = species
+    }
+
+    static definition() {
+        return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.'
+    }
+}
+
+class Deciduous extends Tree {
+    constructor(species, name) {
+        super(species)
+        this.species = species
+        this.name = name
+    }
+
+    static definition() {
+        return `${super.definition()} ${this.name} trees shed their leaves annually.`
+    }
+}
+
+
+class Evergreen extends Tree {
+    constructor(species, name) {
+        super(species)
+        this.species = species
+        this.name = name
+    }
+
+    static definition() {
+        return `${super.definition()} ${this.name}s keep their leaves all year round.`
+    }
+}
